0c2081f547ba5b6bf542a771bb6d97fdb4550147
sunjx <drazen08@163.com>
2022-06-06 13:53:20 +0800

    init

---
+++ src/main/java/org/example/security/controller/DemoController.java
@@ -0,0 +1,18 @@
+package org.example.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/demo")
+@RestController
+public class DemoController {
+
+
+    @GetMapping
+    public String info() {
+        return "demo detail";
+    }
+
+
+}
